ff493d33cbbb9e440ab0792bffd788893d65bbdb
rdas15 <rudrangshu.das@gmail.com>
2021-04-12 21:36:33 +0530

    Fixed 2nd largest element program

--- Arrays/2nd_largest_element.py
+++ Arrays/2nd_largest_element.py
@@ -1,19 +1,20 @@
 # Maximum Function
 def getMaxAndSecondMax(arr, n):
-    large_1 = 0 
-    large_2 = 0    
+    large_1 = 0
+    large_2 = 0   
       
-    for i in range(0,n):
+    for i in range(1,n):
         if arr[i] > large_1:
+            large_2 = large_1
             large_1 = arr[i]
-        elif arr[i] > large_2:
+        elif arr[i] > large_2 and arr[i] != large_1:
             large_2 = arr[i]
 
     print("1st largest num in arr is {}, 2nd largest is {}".format(large_1, large_2))
 
 
 # Driver program
-arr = [12, 4, 78, 42, 56, 2, 5, 16, 8]
+arr = [1, 2, 5, 8, 12, 34, 45]
 n = len(arr)
 getMaxAndSecondMax(arr, n)
 
